62aa9a3a7957541d00613170c157654273d4ecbe
VagnerSilva <vagner23silva@gmail.com>
2019-06-27 22:05:52 +0000

    refactor

--- modulo1/dia1/exercicio1/2.js
+++ modulo1/dia1/exercicio1/2.js
@@ -5,19 +5,16 @@
 
 var destroyer = function (_array, ...param) {
     var list = _array
-    var len = param.length;
-    var novoArray = [];
+    args = arguments;
 
-    param.forEach(function(element) {
-        for( var i = list.length; i--;) {
-            if ( list[i] === element) list.splice(i, 1);
-        }
-    });
+    for( var i = list.length; i--;) {
+        if ( list[i] === args[0] || list[i] === args[1]) list.splice(i, 1);
+    }
     
-
     return list;
 
 }
 
-var result = destroyer([1,3,1,4,6,4],3,4);
+var elements = [1,3,1,4,6,4];
+var result = destroyer(elements, 3, 4);
 console.log(result)
