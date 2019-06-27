5c6070e7bd3c0c515619f58ac9320665c856e6fd
VagnerSilva <vagner23silva@gmail.com>
2019-06-27 22:10:37 +0000

    refactor

--- modulo1/dia1/exercicio1/1.js
+++ modulo1/dia1/exercicio1/1.js
@@ -1,17 +1,17 @@
 // Faça um algoritmo que recebe um Array
 // com dois valores e devolva a soma de todos os numeros entre eles
 
-function compararNumeros(a, b) {
+function compareNumber(a, b) {
     return a - b;
 }  
 
 function sumAll(array_number) {
-    var tamanho =  array_number.length;
-    var orderNumber = array_number.sort(compararNumeros)
+    var len =  array_number.length;
+    var orderNumber = array_number.sort(compareNumber)
     var num1 = orderNumber[0];
     var num2 = orderNumber[1];
     var count = 0;
-    if(tamanho !== 2) {
+    if(len !== 2) {
         console.log('Paramentro deve conter 2 valores');
         return;
     }
@@ -28,5 +28,6 @@ function sumAll(array_number) {
     return count;
 }
 
-var result = sumAll([1,4]);
+var elements = [1,4];
+var result = sumAll(elements);
 console.log(result);
